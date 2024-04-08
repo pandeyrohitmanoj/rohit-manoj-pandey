dfc67e2ae0d5db0652de5ffa6d0881d08f4dd638
pandeyrohitmanoj <rohitpandey20002017@gmail.com>
2024-04-08 15:58:52 +0530

    updated introduction

--- components/introduction.jsx
+++ components/introduction.jsx
@@ -15,7 +15,7 @@ export default function Introduction() {
         <article id='introduction'
          className='py-28  introduction text-white'>
             <div className='max-w-prose px-4 mx-auto text-center'>
-        <h1 className='text-5xl mb-12 euros font-black'>Hi, I’m Rohit. Nice to meet you. </h1>
+        <h1 className='text-3xl mb-12 euros font-black'>Hi, I’m Rohit. Nice to meet you. </h1>
         <p className={`${roboto.className} font-normal text-2xl`}>I have professional experience of 2 years, working as a full-stack developer at Infosys Ltd. India's second largest tech company.
         <br/><br/>I have learned Next JS, MERN stack, SQL Google Cloud services, like pubsub, Google Kubernetes Cluster, Redis Memory Storage, and Cloud Storage, to create dynamic website with Hybrid Rendering, and scalable backend.
         <br/><br/>I have created several projects to solve real-world problems, such as file sharing.</p>
